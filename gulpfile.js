9cf68b97a3ba435cfe0dfa61464ebcfba1755cf7
Matsuura Tomoya <Tomoya@MatsuuraTomoyasMBP.local>
2015-11-23 01:08:04 +0900

    フロントエンドを書き始めた

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,46 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pleeease = require('gulp-pleeease');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
 var plumber = require('gulp-plumber');
-var ejs = require("gulp-ejs");
 
-gulp.task('sass', function(){
-	gulp.src('./_src/scss/*.scss')
+gulp.task('sass', function () {
+	gulp.src('public/stylesheets/*.scss')
 	.pipe(plumber())
-	.pipe(sass({
-		style:'expanded'
-	}))
+	.pipe(sass())
 	.pipe(pleeease({
-		autoprefixer:{
-			browsers:['last 2 versions']
-		},
-		minifier: false
-	}))
-	.pipe(gulp.dest('./dist/css'))
-	.pipe(reload({stream: true}));
+      autoprefixer: { 'browsers': ['last 2 versions', 'ie 6', 'ie 7', 'ie 8', 'Safari 4', 'Android 2.3', 'iOS 4'] },
+      minifier: false
+    }))
+	.pipe(gulp.dest('public/stylesheets'))
+	.pipe(reload({stream:true}));
 });
-gulp.task('server',function(){
-	browserSync({
-		notify:false,
-		server:{
-			baseDir:"./dist",
-			directory: true,
-		}
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null,{
+		files:["public/**/*.*"],
+		proxy:'http://localhost:3000',
+		port:4000,
+		open:false
 	});
 });
 
-gulp.task('watch',function(){
-
-	gulp.watch('_src/scss/*.scss',['sass']);
-	// gulp.watch('./_src/**/*.html',['minify-html']);
-	gulp.watch('dist/*.html',reload);
+gulp.task('default', ['sass', 'browser-sync'], function () {
+	gulp.watch("public/stylesheets/*.scss", ['sass']);
+	gulp.watch(["public/javascripts/*.js", "views/*.ejs"], reload);
 });
-gulp.task('default',['server','watch']);
+
+gulp.task('nodemon', function (cb) {
+	var called = false;
+	return nodemon({
+		script: 'bin/www',
+		ignore:['public/javascripts/*.js']
+	}).on('start', function () {
+		if (!called) {
+			called = true;
+			cb();
+		}
+	});
+});
--- public/javascripts/render.js
+++ public/javascripts/render.js
@@ -1,11 +1,61 @@
-
+var dummymessege = 'thisisatestmessege';
 
 function setup(){
-var binarydata = receiveddata;
+var canvas = createCanvas(800,600);
+canvas.parent("render");
+background(155,140,130,230);
+textSize(18);
+fill(0,200,100);
+rectMode(CORNER);
 }
 
 function draw(){
+  var data = "hoge";
+if(!receiveddata){
+  data = dummymessege;
+}else{
+  data = receiveddata;
+  }
+  var bindata = "";
+  for(var i=0; i < data.length ;i++){
+  bindata= bindata + data.charCodeAt(i).toString(2);
+}
+
+  text(data,100,100);
+  text(bindata,100,150,200,150);
+
 
 }
 
-var modalmenu = document.getelementbyId('#modal');
+// decode functions
+function Decode(data){
+    // this.2binary = 2binary(data);
+//   for(var i; i<data.length;i++){
+//   data.charCodeAt(i);
+// }
+
+// Decode.prototype.2Binary= function 2Binary(data){
+
+}
+
+
+
+var modalmenu = document.getElementById('modalmenu');
+var menubutton = document.getElementById('menubutton');
+
+var closebutton = document.getElementById('closebutton');
+console.log(menubutton);
+menubutton.addEventListener('click',function(){
+  // modalmenu.style.setProperty("display","block");
+
+  modalmenu.style.setProperty("opacity",0.6);
+  modalmenu.style.setProperty("z-index",999);
+
+});
+closebutton.addEventListener('click',function(){
+  // modalmenu.style.setProperty("display","block");
+
+  modalmenu.style.setProperty("opacity",0);
+  modalmenu.style.setProperty("z-index",-999);
+
+});
